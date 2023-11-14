7e4fae7eb1bd95feb4a1f2d02f5908ed8572237f
BedriCanSenel <bedricansenel@gmail.com>
2023-11-14 19:17:40 +0300

    Core ve DTO eklendi.

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -13,11 +13,13 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=CAN;Database=Northwind;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server =CAN;Database = Northwind;Trusted_connection=true;encrypt=false");
         }
-        public DbSet<Product> ProductS { get; set; }       
+        public DbSet<Product> Products { get; set; }       
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
 
 
 
